2ce37321addd5166d0d42686d566fdf99f614781
usriddle <129639843+usriddle@users.noreply.github.com>
2023-06-04 12:32:29 +0900

    Update MatchingController.java
    오류수정

--- Project/src/main/java/com/acorn/project/Controller/MatchingController.java
+++ Project/src/main/java/com/acorn/project/Controller/MatchingController.java
@@ -1,103 +1,107 @@
 package com.acorn.project.Controller;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.acorn.project.DTO.ClickDTO;
-import com.acorn.project.DTO.MatchingDTO;
-import com.acorn.project.service.ChatService;
-import com.acorn.project.service.ClickService;
-import com.acorn.project.service.LikeService;
-import com.acorn.project.service.MatchingService;
+import com.acorn.project.DTO.memberRegDTO;
+import com.acorn.project.service.memberRegService;
+
+
 
 @Controller
-public class MatchingController {
+public class memberJoinController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(memberJoinController.class);
 
-	
 	@Autowired
-	MatchingService mt_service;
+	memberRegService service;
 	
-	@Autowired
-	ClickService ck_service;
-	
-	@Autowired
-	LikeService likeService;
-	
-	@Autowired
-	ChatService chatService;
-	
-	HttpSession session;
-
-	
-	@GetMapping("/test22")
-	public String test(Model model, HttpServletRequest request) {
-		
-		session= request.getSession();
-		String member_id = (String) session.getAttribute("sessionId");
-
-		// 여기입니다 코드 수정한 곳 
-		ClickDTO result4 = mt_service.no_mat_list(member_id);
-		model.addAttribute("result4" , result4);
-		if(result4 !=null) {
-			int result1 = likeService.getLikecnt(result4 .getMember_id());
-			model.addAttribute("result1" , result1);
-			model.addAttribute("result2", result4 .getMember_id());
-		}
-		
-		//
-		if(result4!=null) {
-			ArrayList<String> memlist = mt_service.getMember_info(result4.getMember_id());
-			model.addAttribute("memlist",memlist);
-		}
-		
-		
-		return "test22";
+	@GetMapping("/reg")
+	public String regMember() {
+		return "reg";
 	}
 	
-	//, produces = "application/json; charset=UTF-8"
+	@PostMapping("/reg")
+	public String regMember(memberRegDTO dto) {
 	
-	@PostMapping( value="/test1" )
+		System.out.println(dto);
+		service.memberJoinService(dto);
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value = "/reg/memberIdChk", method = { RequestMethod.POST })
 	@ResponseBody
-	public Object[] A_match(@RequestParam String cnt, @RequestParam String receiver_member_id,
-			@RequestParam String matching_status ,HttpServletRequest request ,HttpServletResponse  response,
-			Model model) {
+	public String memberIdChkPOST(String memberId) throws Exception{
+		//logger.info("들어왔나? " + memberId);
+		//System.out.println(memberId);
+		int result = service.memberIdCheck(memberId);
 		
-				
-		response.setContentType("application/json;charset=UTF-8");
-		response.setCharacterEncoding("UTF-8");
-		 
-		session= request.getSession();
-		String id = (String) session.getAttribute("sessionId");
-		
-		
-		if(matching_status.equals("Y")) {
-			MatchingDTO mDto= new MatchingDTO(null,id,receiver_member_id,matching_status);
-			mt_service.insert_match(mDto);
-			//이제 상대방도 자신에게 like를 보냈는지 확인해서 매칭을 결정
-			if(mt_service.getStatus(receiver_member_id, id).equals("Y")) {
-				chatService.insertChat(new String[]{" ",id,receiver_member_id});
-			}
+		if(result == 0) {
 			
-		}
-		likeService.updateLike(cnt, receiver_member_id);
-		
-		ClickDTO result4 = mt_service.no_mat_list(id);
-		
-		ArrayList<String> memlist = mt_service.getMember_info(result4.getMember_id());
-		
-		
-		return new Object[] {result4,memlist};
-	}
+			return "fail";	// 중복 아이디가 존재
+			
+		} else {
+			
+			return "success";	// 중복 아이디 x
+			
+		}	
+
+	} // memberIdChkPOST() 종료	
 	
+	/* 첨부 파일 업로드 */
+	@PostMapping("/reg/uploadAjaxAction")
+	public void uploadAjaxActionPOST(MultipartFile[] uploadFile, HttpServletRequest request) {
+		
+		
+		
+		logger.info("uploadAjaxActionPOST..........");
+		String uploadFolder = request.getSession().getServletContext().getRealPath("/").concat("resources");
+	
+		String imgPath = "images";
+		File uploadPath = new File(uploadFolder, imgPath);
+		
+		
+		if(!uploadPath.exists()) {
+			uploadPath.mkdirs();
+		}
+			
+		for(MultipartFile multipartfile : uploadFile) {
+			// 파일이름
+			String uploadFileName = multipartfile.getOriginalFilename();
+			
+			/* 파일 위치, 파일 이름을 합친 File 객체 */
+			File saveFile = new File(uploadPath, uploadFileName);
+			logger.info(uploadFileName + " , " + uploadPath);
+			logger.info("파일 이름 : " + multipartfile.getOriginalFilename());
+			logger.info("파일 타입 : " + multipartfile.getContentType());
+			logger.info("파일 크기 : " + multipartfile.getSize());
+			logger.info("===============================================");
+			
+			/* 파일 저장*/
+			try {
+				multipartfile.transferTo(saveFile);
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
